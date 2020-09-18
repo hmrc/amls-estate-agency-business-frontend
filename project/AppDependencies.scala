bd073bafdd7e4fcd1484bd9c1b5c19183a169452
gavinharris43 <45851646+gavinharris43@users.noreply.github.com>
2020-09-18 16:17:18 +0100

    DLS-3036 Library Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,13 +7,13 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "org.reactivemongo" %% "play2-reactivemongo"            % "0.20.3-play26",
     "uk.gov.hmrc"       %% "logback-json-logger"            % "4.8.0",
-    "uk.gov.hmrc"       %% "govuk-template"                 % "5.55.0-play-26",
+    "uk.gov.hmrc"       %% "govuk-template"                 % "5.56.0-play-26",
     "uk.gov.hmrc"       %% "play-health"                    % "3.15.0-play-26",
     "uk.gov.hmrc"       %% "play-ui"                        % "8.12.0-play-26",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.3.0-play-26",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-26"     % "2.25.0",
     "uk.gov.hmrc"       %% "play-whitelist-filter"          % "3.4.0-play-26",
-    "uk.gov.hmrc"       %% "auth-client"                    % "3.0.0-play-26"
+    "uk.gov.hmrc"       %% "auth-client"                    % "3.1.0-play-26"
   )
 
   val test = Seq(
