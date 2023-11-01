fca419081e67335e6b26e7572086bca8ea01fef4
jpatton638 <jpatton638@users.noreply.github.com>
2023-11-01 11:34:26 +0000

    DDCYLS-3717 | Alter construction of referrer URL for contact-frontend

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -25,11 +25,19 @@ import java.net.URLEncoder
 @Singleton
 class FrontendAppConfig @Inject() (configuration: Configuration) {
 
+  private def baseUrl(serviceName: String): String = {
+    val protocol = configuration.getOptional[String](s"microservice.services.$serviceName.protocol").getOrElse("http")
+    val host = configuration.get[String](s"microservice.services.$serviceName.host")
+    val port = configuration.get[String](s"microservice.services.$serviceName.port")
+    s"$protocol://$host:$port"
+  }
+
   val timeoutSeconds = configuration.get[String](s"timeout.seconds")
   val timeoutCountdown = configuration.get[String](s"timeout.countdown")
   def reportAProblemNonJSUrl(implicit request: Request[_]): String = {
     configuration.get[String]("microservice.services.contact-frontend.report-problem-url.non-js") +
-    "&referrerUrl=" + URLEncoder.encode(request.host + request.uri, "utf-8")
+    "&referrerUrl=" +
+    URLEncoder.encode(baseUrl("amls-estate-agency-business-frontend") + request.uri, "utf-8")
   }
 
   val amlsFrontendBaseUrl = configuration.get[String](s"microservice.services.amls-frontend.url")
