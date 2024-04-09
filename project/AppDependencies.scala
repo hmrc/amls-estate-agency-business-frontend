822cebec229d776eb505c33f68d53d7accc31dcc
jpatton638 <jpatton638@users.noreply.github.com>
2024-04-09 16:33:32 +0100

    DDCYLS-4955 | Upgrade to Tudor Crown

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "8.4.0"
+  val bootstrapVersion = "8.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "2.0.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "8.4.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "8.5.0",
     "commons-codec"     %  "commons-codec"                         % "1.16.0"
   )
 
