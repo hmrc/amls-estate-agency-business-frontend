a831d2026d9f59649cbbc95aa7c949c154bf8da2
Bhanu <177983246+bhanuparsad@users.noreply.github.com>
2026-02-16 07:18:31 +0000

    vulnerability fixes (#97)
    Co-authored-by: bhanu-nov1989 <177983246+bhanu-nov1989@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,19 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.19.0"
+  val bootstrapVersion = "10.5.0"
   val playVersion      = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"  %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
+    "uk.gov.hmrc"  %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
     "uk.gov.hmrc"  %% s"play-conditional-form-mapping-$playVersion" % "3.4.0",
-    "uk.gov.hmrc"  %% s"play-frontend-hmrc-$playVersion"            % "12.22.0",
-    "commons-codec" % "commons-codec"                               % "1.17.2"
+    "uk.gov.hmrc"  %% s"play-frontend-hmrc-$playVersion"            % "12.31.0",
+    "commons-codec" % "commons-codec"                               % "1.17.2",
+    "org.apache.commons"   % "commons-lang3"               % "3.18.0",
+    "ch.qos.logback"       % "logback-core"                % "1.5.27",
+    "ch.qos.logback"       % "logback-classic"             % "1.5.27",
+    "at.yawk.lz4"          %  "lz4-java"                   % "1.10.3"
   )
 
   val test: Seq[ModuleID] = Seq(
